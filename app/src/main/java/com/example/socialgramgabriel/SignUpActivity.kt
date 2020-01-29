e8bd712e1fedf0973c490375bfb83d4cc4601ad1
Gabriel <tandohtakyie@hotmail.com>
2020-01-29 14:27:33 +0000

    Added log in and register screens

---
+++ app/src/main/java/com/example/socialgramgabriel/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package com.example.socialgramgabriel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+       signIn_link_btn.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+    }
+}
