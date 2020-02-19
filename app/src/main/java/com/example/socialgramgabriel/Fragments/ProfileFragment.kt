b095076c7a2f95a05362840520827c806ee930e5
Gabriel <tandohtakyie@hotmail.com>
2020-02-19 12:12:11 +0000

    try to display the saved images but not working as expected,nothing show
    23

--- app/src/main/java/com/example/socialgramgabriel/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/socialgramgabriel/Fragments/ProfileFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -39,6 +40,11 @@ class ProfileFragment : Fragment() {
     var postList: List<Post>? = null
     var myImagesAdapter: MyImagesAdapter? = null
 
+
+    var myImagesAdapterSavedImg: MyImagesAdapter? = null
+    var postListSaved: List<Post>? = null
+    var mySavesImag: List<String>? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -60,6 +66,8 @@ class ProfileFragment : Fragment() {
             checkFollowAndFollowingButtonStatus()
         }
 
+
+        // recycler view for uploaded images
         var recyclerViewUploadImages: RecyclerView
         recyclerViewUploadImages = view.findViewById(R.id.recycler_view_upload_pic)
         recyclerViewUploadImages.setHasFixedSize(true)
@@ -69,6 +77,37 @@ class ProfileFragment : Fragment() {
         postList = ArrayList()
         myImagesAdapter = context?.let { MyImagesAdapter(it, postList as ArrayList<Post>) }
         recyclerViewUploadImages.adapter = myImagesAdapter
+
+
+        // recycler view for uploaded images
+        var recyclerViewSavedImages: RecyclerView
+        recyclerViewSavedImages = view.findViewById(R.id.recycler_view_saved_pic)
+        recyclerViewSavedImages.setHasFixedSize(true)
+        val linearLayoutManager2: LinearLayoutManager = GridLayoutManager(context, 3)
+        recyclerViewSavedImages.layoutManager = linearLayoutManager2
+
+        postListSaved = ArrayList()
+        myImagesAdapterSavedImg =
+            context?.let { MyImagesAdapter(it, postListSaved as ArrayList<Post>) }
+        recyclerViewSavedImages.adapter = myImagesAdapterSavedImg
+
+
+        recyclerViewSavedImages.visibility = View.GONE
+        recyclerViewUploadImages.visibility = View.VISIBLE
+
+        var uploadedImagesButton: ImageButton
+        uploadedImagesButton = view.findViewById(R.id.images_grid_view_btn)
+        uploadedImagesButton.setOnClickListener {
+            recyclerViewSavedImages.visibility = View.GONE
+            recyclerViewUploadImages.visibility = View.VISIBLE
+        }
+
+        var savedImagesButton: ImageButton
+        savedImagesButton = view.findViewById(R.id.images_save_btn)
+        savedImagesButton.setOnClickListener {
+            recyclerViewSavedImages.visibility = View.VISIBLE
+            recyclerViewUploadImages.visibility = View.GONE
+        }
 
 
         view.edit_account_settings_btn.setOnClickListener {
@@ -115,6 +154,7 @@ class ProfileFragment : Fragment() {
         getTotalNumberOfPosts()
         getUserInfo()
         myPhotos()
+        mySaves()
 
         return view
     }
@@ -182,15 +222,15 @@ class ProfileFragment : Fragment() {
         })
     }
 
-    private fun myPhotos(){
+    private fun myPhotos() {
         val postRef = FirebaseDatabase.getInstance().reference.child("Posts")
-        postRef.addValueEventListener(object : ValueEventListener{
+        postRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     (postList as ArrayList<Post>).clear()
-                    for (snapshot in dataSnapshot.children){
+                    for (snapshot in dataSnapshot.children) {
                         val post = snapshot.getValue(Post::class.java)!!
-                        if (post.getPublisher().equals(profileId)){
+                        if (post.getPublisher().equals(profileId)) {
                             (postList as ArrayList<Post>).add(post)
                         }
                         Collections.reverse(postList)
@@ -198,11 +238,60 @@ class ProfileFragment : Fragment() {
                     }
                 }
             }
+
             override fun onCancelled(p0: DatabaseError) {
 
             }
 
 
+        })
+    }
+
+    private fun mySaves() {
+        mySavesImag = ArrayList()
+        val savedRef = FirebaseDatabase.getInstance().reference
+            .child("Saves")
+            .child(fireBaseUser.uid)
+
+        savedRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (snapshot in dataSnapshot.children) {
+                        (mySavesImag as ArrayList<String>).add(snapshot.key!!)
+                    }
+                    readSavedImagesData()
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun readSavedImagesData() {
+        val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
+
+        postsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    (postListSaved as ArrayList<Post>).clear()
+
+                    for (snapshot in dataSnapshot.children) {
+                        val post = snapshot.getValue(Post::class.java)
+                        for (key in mySavesImag!!) {
+                            if (post!!.getPostID() == key) {
+                                (postListSaved as ArrayList<Post>).add(post!!)
+                            }
+                        }
+                    }
+                    myImagesAdapterSavedImg!!.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
         })
     }
 
@@ -216,7 +305,8 @@ class ProfileFragment : Fragment() {
 //                }
                 if (dataSnapshot.exists()) {
                     val user = dataSnapshot.getValue<User>(User::class.java)
-                    Picasso.get().load(user!!.getImage()).placeholder(R.drawable.defaultprofileimage)
+                    Picasso.get().load(user!!.getImage())
+                        .placeholder(R.drawable.defaultprofileimage)
                         .into(view?.pro_image_profile_frag)
                     view?.profile_fragment_username?.text = user!!.getUsername()
                     view?.full_name_profile_frag?.text = user!!.getFullName()
@@ -230,16 +320,16 @@ class ProfileFragment : Fragment() {
         })
     }
 
-    private fun getTotalNumberOfPosts(){
+    private fun getTotalNumberOfPosts() {
         val postRef = FirebaseDatabase.getInstance().reference.child("Posts")
-        postRef.addValueEventListener(object : ValueEventListener{
+        postRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if(dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     var postCounter = 0
 
-                    for (snapshot in dataSnapshot.children){
+                    for (snapshot in dataSnapshot.children) {
                         val post = snapshot.getValue(Post::class.java)
-                        if (post!!.getPublisher() == profileId){
+                        if (post!!.getPublisher() == profileId) {
                             postCounter++
                         }
                     }
@@ -247,6 +337,7 @@ class ProfileFragment : Fragment() {
                     total_posts.text = " " + postCounter
                 }
             }
+
             override fun onCancelled(dataSnapshot: DatabaseError) {
 
             }
