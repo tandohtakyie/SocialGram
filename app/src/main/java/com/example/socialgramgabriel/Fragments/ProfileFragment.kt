b1da48d9934870e9d2fbc368fd974824ead2828b
Gabriel <tandohtakyie@hotmail.com>
2020-02-21 11:03:12 +0000

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1f37a9580b4fc7fda536bd83d332deac1af071aa.

--- app/src/main/java/com/example/socialgramgabriel/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/socialgramgabriel/Fragments/ProfileFragment.kt
@@ -111,7 +111,6 @@ class ProfileFragment : Fragment() {
         }
 
 
-<<<<<<< HEAD
         view.total_followers.setOnClickListener{
             val intent = Intent(context, ShowUsersActivity::class.java)
             intent.putExtra("id", profileId)
@@ -257,7 +256,6 @@ class ProfileFragment : Fragment() {
                 }
             }
 
-<<<<<<< HEAD
             override fun onCancelled(p0: DatabaseError) {
 
             }
@@ -281,54 +279,6 @@ class ProfileFragment : Fragment() {
                     readSavedImagesData()
                 }
             }
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-        })
-    }
-
-    private fun readSavedImagesData() {
-        val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
-
-        postsRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    (postListSaved as ArrayList<Post>).clear()
-
-                    for (snapshot in dataSnapshot.children) {
-                        val post = snapshot.getValue(Post::class.java)
-                        for (key in mySavesImag!!) {
-                            if (post!!.getPostID() == key) {
-                                (postListSaved as ArrayList<Post>).add(post!!)
-                            }
-                        }
-                    }
-                    myImagesAdapterSavedImg!!.notifyDataSetChanged()
-                }
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-        })
-    }
-
-    private fun mySaves() {
-        mySavesImag = ArrayList()
-        val savedRef = FirebaseDatabase.getInstance().reference
-            .child("Saves")
-            .child(fireBaseUser.uid)
-
-        savedRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    for (snapshot in dataSnapshot.children) {
-                        (mySavesImag as ArrayList<String>).add(snapshot.key!!)
-                    }
-                    readSavedImagesData()
-                }
-            }
-
             override fun onCancelled(p0: DatabaseError) {
 
             }
